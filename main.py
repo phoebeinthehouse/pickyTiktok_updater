5b937aaa8145cc37cf1e5df19502206eda165ac2
phoebeinthehouse <chaeyeon.lim@manyo.co.kr>
2026-01-09 09:19:08 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+print("=== GitHub Actions test run ===")
+print("Current time:", datetime.utcnow().isoformat(), "UTC")
+print("If you see this, GitHub Actions is working.")
